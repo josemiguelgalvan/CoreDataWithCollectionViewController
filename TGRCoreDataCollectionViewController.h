f0e87e56bcbc492a2c648b9e392a95279157b725
José Miguel Galván <josemiguelgalvan@gmail.com>
2015-04-22 08:29:21 +0100

    Create TGRCoreDataCollectionViewController.h

---
+++ TGRCoreDataCollectionViewController.h
@@ -0,0 +1,33 @@
+//
+//  TGRCoreDataCollectionViewController.h
+//  CoreDataTest
+//
+//  Created by José Miguel Galván on 12/2/15.
+//  Copyright (c) 2015 Tagorito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGRCoreDataSimpleCollectionViewController : UICollectionViewController
+
+@property (strong, nonatomic) NSManagedObjectContext *context;
+
++(instancetype)coreDataCollectionViewControllerWithFetchRequest:(NSFetchRequest *)request
+                                         inManagedObjectContext:(NSManagedObjectContext *) context;
+
++(instancetype)coreDataCollectionViewControllerWithFetchRequest:(NSFetchRequest *)request
+                                           collectionViewLayout:(UICollectionViewLayout *) layout
+                                         inManagedObjectContext:(NSManagedObjectContext *) context;
+
+// designated
+-(id) initWithFetchRequest:(NSFetchRequest *)request
+      collectionViewLayout:(UICollectionViewLayout *) layout
+    inManagedObjectContext:(NSManagedObjectContext *) context;
+
+-(id) initWithFetchRequest:(NSFetchRequest *)request
+    inManagedObjectContext:(NSManagedObjectContext *) context;
+
+
+-(id) objectAtIndexPath:(NSIndexPath*)indexPath;
+
+@end
